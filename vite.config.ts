8bafa92381b161af28a23bf26823f1f55c997db3
Vincent Baskerville <vincent_baskerville1@homedepot.com>
2026-01-23 16:26:51 -0500

    Force Vite base to root

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/store-xm-dashboard/' : '/',
+export default defineConfig(() => ({
+  base: '/',
   plugins: [
     // The React and Tailwind plugins are both required for Make, even if
     // Tailwind is not being actively used – do not remove them
